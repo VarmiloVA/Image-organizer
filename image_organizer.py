97189fe0277cb9fa8b817ca75a52646b6e23b297
Daniel Atanasov <varmilo.blue@protonmail.com>
2022-07-27 16:22:56 +0200

    .gitignore added

--- image_organizer.py
+++ image_organizer.py
@@ -1,13 +1,15 @@
-from PIL import Image # pip install pillow
+from PIL import Image
 import os
 
-downloads_fldr = "C:/Users/varmi/Downloads/"
-images_fldr = "C:/Users/varmi/OneDrive/Pictures/Saved_by_Python/"
+downloadsFolder = "C:/Users/varmi/Downloads/"
+picturesFolder = "pictures/"
 
-if __name__ == '__main__':
-    for filename in os.listdir(downloads_fldr):
-        name, extension = os.path.splitext(downloads_fldr + filename)
-        if extension in ['.jpg', '.png', '.jpeg']:
-            picture = Image.open(downloads_fldr + filename)
-            picture.save(downloads_fldr + "compressed_"+filename, optimize=True, quality=60)
+if __name__ == "__main__":
+    for filename in os.listdir(downloadsFolder):
+        name, extension = os.path.splitext(downloadsFolder + filename)
+
+        if extension in [".jpg", ".jpeg", ".png"]:
+            picture = Image.open(downloadsFolder + filename)
+            picture.save(picturesFolder + "compressed_"+filename, optimize=True, quality=60)
+            os.remove(downloadsFolder + filename)
             print(name + ": " + extension)
