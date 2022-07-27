68deab1d1255061c0d6703c47e6d64160daaeb03
Daniel Atanasov <varmilo.blue@protonmail.com>
2022-07-27 16:09:19 +0200

    Dumb Sintax err solved

--- image_organizer.py
+++ image_organizer.py
@@ -1,8 +1,8 @@
 from PIL import Image # pip install pillow
 import os
 
-downloads_fldr = 'C:\Users\varmi\Downloads'
-images_fldr = "C:\Users\varmi\OneDrive\Pictures\Saved_by_Python"
+downloads_fldr = "C:/Users/varmi/Downloads/"
+images_fldr = "C:/Users/varmi/OneDrive/Pictures/Saved_by_Python/"
 
 if __name__ == '__main__':
     for filename in os.listdir(downloads_fldr):
